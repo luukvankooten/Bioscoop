0511eca123408d025793e945aabe17e18047e805
Luuk van Kooten <luuk@Mac-mini-van-Luuk.local>
2022-02-04 09:55:21 +0100

    Initial commit

---
+++ Bioscoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bioscoop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
